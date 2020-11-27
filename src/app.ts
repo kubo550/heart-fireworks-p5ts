c966c5fe57fb67ccd7ebd7b83ddd2cd0e1196cd7
powerty <43968748+kubo550@users.noreply.github.com>
2020-11-27 13:09:56 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,15 @@ import "p5/lib/addons/p5.dom";
 
 import Firework from "./Firework";
 
-let gravity: P5.Vector;
-
 const sketch = (p5: P5) => {
   let fireworks: Firework[] = [];
 
   p5.setup = () => {
     p5.createCanvas(window.innerWidth, window.innerHeight);
-    gravity = p5.createVector(0, 0.05);
+    setInterval(() => fireworks.push(new Firework(p5)), 1800);
   };
 
   p5.draw = () => {
-    if (p5.random(1) > 0.99) {
-      fireworks.push(new Firework(p5, gravity));
-    }
-
     p5.background("black");
     fireworks.forEach((f) => f.update());
     fireworks = fireworks.filter((p) => !p.isDead());
